67c50c61c757b4a9293a4ccf9482fda4e164310a
Kumriye Yıldız <kumryldz@gmail.com>
2021-02-10 21:10:56 +0300

    20 Ocak 2021

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string adi = "Engin";
+            int yas = 36;
+
+            Kurs kurs1 = new Kurs();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+
+        public string Egitmen { get; set; }
+
+        public string IzlenmeOrani { get; set; }
+
+    }
+
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=1; i<10; i++) 
+            {
+                Console.WriteLine(i);
+            }
+
+            
+        }
+    }
+}
